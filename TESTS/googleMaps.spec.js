4ae7528b65270369d85b6743b6c197092b566c30
Tereza Winterová <tewinterova@seznam.cz>
2026-04-11 19:28:23 +0200

    rawTest

---
+++ TESTS/googleMaps.spec.js
@@ -0,0 +1,61 @@
+const { remote } = require('webdriverio');
+
+const capabilities = {
+    platformName: 'Android',
+    'appium:automationName': 'UiAutomator2',
+    'appium:deviceName': 'Redmi Note 8 Pro',
+    'appium:appPackage': 'com.google.android.apps.maps',
+    'appium:appActivity': 'com.google.android.maps.MapsActivity',
+    'appium:noReset': true,
+};
+
+async function main() {
+    const driver = await remote({
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: 4723,
+        capabilities
+    });
+
+    // Setting the text into the search box
+    const searchBox = await driver.$('android=new UiSelector().text("Zkuste")');
+    await searchBox.click();
+    await searchBox.setValue('Packeta Group');
+    await driver.pressKeyCode(66) // presses Enter key
+
+    // Waiting for loading and verification of company name
+    const resultTitle = await driver.$('//*contains(@text, "Packeta")]');
+    await driver.waitUntil(async () => {
+        return await resultTitle.isDisplayed();
+    }, {
+        timeoutMsg :'Error : Search result Packeta has not been displayed in due time.'
+    });
+
+    const actualName = await resultTitle.getText();
+    const isCorrectName = actualName.includes('Packeta Group') || actualName.includes('Packeta s.r.o');
+    if (!isCorrectName) {
+        throw new Error(`Error : Search result has incorrect name. Expected: "Packeta Group" or "Packeta s.r.o", Actual: "${actualName}"`);
+    }
+    console.log(`Test passed: Search result has correct name: ${actualName}`);
+
+    // Scrolling to the address
+    const addressSnippet = "Českomoravská 2408";
+
+    const scrollSelector = `new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().textContains("${addressSnippet}"))`;
+    
+    await $(`android=${scrollSelector}`);
+
+    const addressElement = await $('//android.widget.TextView[contains(@text, "Českomoravská")]');
+
+    const actualAddress = await addressElement.getText();
+
+    const expectedAddress = "Českomoravská 2408, 190 00 Praha 9-Libeň";
+
+    console.log(`Actual address is : ${actualAddress}`);
+
+    if (actualAddress.includes(expectedAddress)) {
+        console.log("Test passed: Address is correct.");
+    } else {
+        throw new Error(`Error: Address is incorrect. Expected: "${expectedAddress}", Actual: "${actualAddress}"`);
+    }
+}
